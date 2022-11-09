11cd520995ec5c96930c6a62e608f2125fce929f
Zephyr <90854121+WhistlingZephyr@users.noreply.github.com>
2022-11-09 10:14:47 +0530

    feat: add url validator

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -13,3 +13,31 @@ export function withScheme(url: string, scheme: string): string {
 
     return url;
 }
+
+function isUrl(string: string): boolean {
+    if (typeof string !== 'string') {
+        throw new TypeError('Expected a string');
+    }
+
+    try {
+        const _url = new URL(string);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+export function validate(url: string, ...args: string[]): boolean {
+    const result = isUrl(url);
+    if (!result) {
+        return false;
+    }
+
+    for (const arg of args) {
+        if (!url.includes(arg)) {
+            return false;
+        }
+    }
+
+    return true;
+}

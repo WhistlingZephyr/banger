e9e897b9ec81ecb8beb8afede9c57162d6d273bd
Zephyr <90854121+WhistlingZephyr@users.noreply.github.com>
2022-11-09 13:53:59 +0530

    chore: add validation to lucky bang updater

--- src/helpers/lucky-bang.ts
+++ src/helpers/lucky-bang.ts
@@ -1,10 +1,19 @@
 import {loadConfig, updateConfig} from '../utils/config';
+import {isUrl} from '../utils/url';
 
 export async function getLuckyBangUrl(): Promise<string> {
     return loadConfig<string>('lucky-bang-url', 'https://duckduckgo.com/?q=!+%s');
 }
 
 export async function updateLuckyBangUrl(url: string): Promise<void> {
+    if (!isUrl(url)) {
+        throw new Error(`Invalid URL "${url}"`);
+    }
+
+    if (!url.includes('%s')) {
+        throw new Error(`URL "${url}" doesn't include "%s"`);
+    }
+
     await updateConfig({
         'lucky-bang-url': url,
     });
